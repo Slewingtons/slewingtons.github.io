b46ec30a9d38924833e2b063fbe1c225b301f66d
Sam Lewis <sam.anders.lewis@gmail.com>
2025-08-23 18:03:59 +1000

    CW now handled by JS.
    The content warning system now uses JavaScript instead of a pure CSS solution.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Attempt to initialise the content warning.
+var cw = document.getElementById("cw");
+
+// Show content warning if it exists.
+if (cw !== null) {
+	// Allow the user to click through the content warning.
+	cw.addEventListener("click", hideCW);
+	
+	// Do not show the content warning if the URL hash is #cw.
+	if (window.location.hash !== "#cw") {
+		cw.style.visibility = "visible";
+	}
+}
+
+// Hide the content warning.
+function hideCW() {
+	if (cw !== null) {
+		cw.style.visibility = "hidden";
+
+		// Add the URL hash #cw.
+		history.replaceState(null, "", "#cw");
+	}
+}
